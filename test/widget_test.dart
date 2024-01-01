e2a64e419f40c341f630e26600a6713beb216e62
hongseop <a01082372487@gmail.com>
2024-01-01 15:14:53 +0900

    install :: get다운로드

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:soldout/main.dart';
+import 'package:instargram/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
