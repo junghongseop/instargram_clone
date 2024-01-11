bffb1f65d2b8811098e5972cc44ef05b03fd1804
junghongseop <a01082372487@gmail.com>
2024-01-11 17:32:29 +0900

    fix :: 사진 경로 변경

--- lib/src/components/post_widget.dart
+++ lib/src/components/post_widget.dart
@@ -13,7 +13,7 @@ class PostWidget extends StatelessWidget {
         children: [
           AvatarWidget(
             thumbPath:
-                'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/373691152_3618951701670615_3597480945046137994_n.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=wEZP4ddZaCwAX-Hb8dF&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAtOYb3BGm5SllqXSLub8cdqK1E4R7KXUQjIDZHljqC8g&oe=6599145E&_nc_sid=8b3546',
+                'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/373691152_3618951701670615_3597480945046137994_n.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=mkMZDE2j7S4AX-eiKj4&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAmpFUf-EFe8mK9bs6MKjmiqkgUFGMb0zmKNK_NG4S-GA&oe=65A4F1DE&_nc_sid=8b3546',
             type: AvatarType.TYPE3,
             nickname: 'wid_ghdtjq',
             size: 40,
