a3471a94398c50262769a8a333e6baaf7afe7b1e
junghongseop <a01082372487@gmail.com>
2024-01-11 22:19:14 +0900

    finish :: 게시물 퍼블리싱

--- lib/src/components/post_widget.dart
+++ lib/src/components/post_widget.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:expandable_text/expandable_text.dart';
 import 'package:flutter/material.dart';
 import 'package:instargram/src/components/avatar_widget.dart';
 import 'package:instargram/src/components/image_data.dart';
@@ -20,7 +22,7 @@ class PostWidget extends StatelessWidget {
           ),
           GestureDetector(
             onTap: () {},
-            child : Padding(
+            child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: ImageData(
                 IconsPath.postMoreIcon,
@@ -33,18 +35,112 @@ class PostWidget extends StatelessWidget {
     );
   }
 
+  Widget _image() {
+    return CachedNetworkImage(
+        imageUrl:
+            'https://t4.ftcdn.net/jpg/02/96/15/35/360_F_296153501_B34baBHDkFXbl5RmzxpiOumF4LHGCvAE.jpg');
+  }
+
+  Widget _infoCount() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              ImageData(
+                IconsPath.likeOffIcon,
+                width: 65,
+              ),
+              const SizedBox(width: 15),
+              ImageData(
+                IconsPath.replyIcon,
+                width: 60,
+              ),
+              const SizedBox(width: 15),
+              ImageData(
+                IconsPath.directMessage,
+                width: 55,
+              ),
+            ],
+          ),
+          ImageData(IconsPath.bookMarkOffIcon, width: 50),
+        ],
+      ),
+    );
+  }
+
+  Widget _infoDescription() {
+    return const Padding(
+      padding: EdgeInsets.only(left: 15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            '좋아요 150개',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          ExpandableText(
+            '테스트용 게시물입니다.\n테스트용 게시물입니다.\n테스트용 게시물입니다.',
+            prefixText: 'wid_ghdtjq',
+            // 사용자의 ID를 클릭하면 마이페이지로 이동
+            // onPrefixTap: () {
+            //   print("사용자의 마이페이지로 이동");
+            // },
+            prefixStyle: TextStyle(fontWeight: FontWeight.bold),
+            expandText: '더보기',
+            expandOnTextTap: true,
+            // 최대로 볼 수 있는 텍스트 줄
+            // maxLines: 3,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _replyTextBtn() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 15.0, top: 5.0),
+      child: GestureDetector(
+        onTap: () {},
+        child: const Text(
+          '댓글 10개 모두 보기',
+          style: TextStyle(color: Colors.grey, fontSize: 13),
+        ),
+      ),
+    );
+  }
+
+  Widget _dateAgo() {
+    return const Padding(
+      padding: EdgeInsets.only(left: 15.0, top: 2.0),
+      child: Text(
+        '1일 전',
+        style: TextStyle(
+          color: Colors.grey,
+          fontSize: 11,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(top: 20),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           _header(),
-          // _image(),
-          // _infoCount(),
-          // _infoDescription(),
-          // _replyTextBtn(),
-          // _dateAgo(),
+          const SizedBox(height: 15),
+          _image(),
+          const SizedBox(height: 15),
+          _infoCount(),
+          const SizedBox(height: 5),
+          _infoDescription(),
+          _replyTextBtn(),
+          _dateAgo(),
         ],
       ),
     );
