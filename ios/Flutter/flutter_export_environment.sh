7ab75037e236bcbe3677eeb68c2f12f995ffcb27
junghongseop <a01082372487@gmail.com>
2024-01-12 10:18:26 +0900

    set :: ios device

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\sub\Downloads\flutter_windows_3.13.8-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\sub\Desktop\soldout_clone\soldout"
+export "FLUTTER_ROOT=/Users/wid_ghdtjq/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/wid_ghdtjq/StudioProjects/instargram_clone"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=/Users/wid_ghdtjq/StudioProjects/instargram_clone/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8zZjNlNTYwMjM2NTM5YjdlMjcwMmY1YWM3OTBiMmE0NjkxYjMyZDQ5Lw=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/wid_ghdtjq/StudioProjects/instargram_clone/.dart_tool/package_config.json"
