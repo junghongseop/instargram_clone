d1c7b0dc72abaf5332396bc5bbc8ec15677870b3
junghongseop <a01082372487@gmail.com>
2024-01-15 19:37:17 +0900

    add :: search_appbar

--- lib/src/pages/search/search_focus.dart
+++ lib/src/pages/search/search_focus.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:instargram/src/components/image_data.dart';
 
 class SearchFocus extends StatefulWidget {
   const SearchFocus({Key? key}) : super(key: key);
@@ -10,6 +12,40 @@ class SearchFocus extends StatefulWidget {
 class _SearchFocusState extends State<SearchFocus> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: Get.back,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ImageData(
+              IconsPath.backBtnIcon,
+            ),
+          ),
+        ),
+        titleSpacing: 0,
+        title: Container(
+          margin: const EdgeInsets.only(right: 15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6),
+            color: const Color(0xffefefef),
+          ),
+          child: const TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: "검색",
+              contentPadding: EdgeInsets.only(right: 15, top: 7, bottom: 7),
+              isDense: true,
+            ),
+          ),
+        ),
+      ),
+      body: const SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
