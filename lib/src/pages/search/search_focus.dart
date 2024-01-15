689108418e8120047124ced18df34d58a5391de2
junghongseop <a01082372487@gmail.com>
2024-01-15 19:28:22 +0900

    apply :: SearchFocus.dart

---
+++ lib/src/pages/search/search_focus.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchFocus extends StatefulWidget {
+  const SearchFocus({Key? key}) : super(key: key);
+
+  @override
+  State<SearchFocus> createState() => _SearchFocusState();
+}
+
+class _SearchFocusState extends State<SearchFocus> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
