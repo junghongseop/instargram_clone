671a4e3be3c1a7eb2048c48e1bd0adaeb412ab78
junghongseop <a01082372487@gmail.com>
2024-02-17 16:26:08 +0900

    add :: 프로필, 게시물, 팔로우, 팔로워 마이페이지에 추가

--- lib/src/pages/mypage.dart
+++ lib/src/pages/mypage.dart
@@ -1,8 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:instargram/src/components/avatar_widget.dart';
 import 'package:instargram/src/components/image_data.dart';
 
 class MyPage extends StatelessWidget {
   const MyPage({Key? key}) : super(key: key);
+
+  Widget _statisticsOne(String title, int value) {
+    return Column(
+      children: [
+        Text(
+          value.toString(),
+          style: const TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 15,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _information() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 14.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            children: [
+              AvatarWidget(
+                thumbPath:
+                    'https://pzip.kr/wp-content/uploads/2023/09/image-76.png',
+                type: AvatarType.TYPE2,
+                size: 100,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: _statisticsOne('게시물', 12),
+                    ),
+                    Expanded(
+                      child: _statisticsOne('팔로워', 264),
+                    ),
+                    Expanded(
+                      child: _statisticsOne('팔로우', 261),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            '정홍섭',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black,
+            ),
+          ),
+          const Text(
+            'bssm 3기',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _menu() {
+    return
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +124,10 @@ class MyPage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: [],
+          children: [
+            _information(),
+            _menu(),
+          ],
         ),
       ),
     );
