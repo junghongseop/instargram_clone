443b250e97849ad58ed86aa405073dd49390a672
junghongseop <a01082372487@gmail.com>
2024-02-17 15:54:02 +0900

    add :: mypage 상단바

---
+++ lib/src/pages/mypage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:instargram/src/components/image_data.dart';
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: false,
+        elevation: 0,
+        title: const Text(
+          'wid_ghdtjq',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: ImageData(
+              IconsPath.uploadIcon,
+              width: 60,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: GestureDetector(
+              onTap: () {},
+              child: ImageData(
+                IconsPath.menuIcon,
+                width: 60,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
