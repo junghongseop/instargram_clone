9be050ee6ac5593518bcb5158f94db99b0d65ac5
junghongseop <a01082372487@gmail.com>
2024-02-19 19:57:13 +0900

    add :: 파이어베이스 연동, 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:instargram/src/app.dart';
+import 'package:instargram/firebase_options.dart';
 import 'package:instargram/src/binding/init_bindings.dart';
+import 'package:instargram/src/root.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -23,7 +27,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       initialBinding: InitBinding(),
-      home: const App(),
+      home: const Root(),
     );
   }
 }
