da274d7ac5b92fb10826c75a3c5dda02248e3a6f
junghongseop <a01082372487@gmail.com>
2024-02-21 21:10:20 +0900

    fix :: 플러그인 버전

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/wid_ghdtjq/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/wid_ghdtjq/Documents/instargram"
+export "FLUTTER_APPLICATION_PATH=/Users/wid_ghdtjq/StudioProjects/instargram_clone"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
