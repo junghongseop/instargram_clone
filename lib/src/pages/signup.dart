6c08194ca081ce7b72e17ef8a3721254fd4d4bf7
junghongseop <a01082372487@gmail.com>
2024-02-23 00:43:23 +0900

    add :: 프로필 변경 화면

--- lib/src/pages/signup.dart
+++ lib/src/pages/signup.dart
@@ -1,10 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:instargram/src/controller/auth_controller.dart';
+import 'package:instargram/src/models/user_model.dart';
 
-class SignupPage extends StatelessWidget {
-  const SignupPage({super.key});
+class SignupPage extends StatefulWidget {
+  final String uid;
+
+  const SignupPage({super.key, required this.uid});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  TextEditingController nicknameController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  Widget _avatar() {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 5.0,
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: SizedBox(
+            width: 100,
+            height: 100,
+            child: Image.asset(
+              'assets/default_image.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.white,
+            elevation: 0.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          onPressed: () {},
+          child: const Text(
+            'change image',
+            style: TextStyle(
+              color: Colors.blue,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _nickname() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      child: TextField(
+        controller: nicknameController,
+        decoration: const InputDecoration(
+          contentPadding: EdgeInsets.all(10),
+          hintText: 'nickname',
+        ),
+      ),
+    );
+  }
+
+  Widget _description() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      child: TextField(
+        controller: nicknameController,
+        decoration: const InputDecoration(
+          contentPadding: EdgeInsets.all(10),
+          hintText: 'description',
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        title: const Text(
+          '완료',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _avatar(),
+            _nickname(),
+            _description(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 40.0),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0),
+            ),
+          ),
+          onPressed: () {
+            var signupUser = UserModel(
+              uid: widget.uid,
+              nickname: nicknameController.text,
+              description: descriptionController.text,
+            );
+            AuthController.to.signup(signupUser);
+          },
+          child: const Text(
+            'Change',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
